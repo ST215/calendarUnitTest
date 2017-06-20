74d5c76081ca502a0614c35c36f1fe4745a108bf
Stanley Griggs II <shg5004@gmail.com>
2017-06-20 18:58:06 -0400

    Working Commit - Calendar Settings
    Created Calendar Settings Class

---
+++ src/CalendarSettings.java
@@ -0,0 +1,21 @@
+import java.text.SimpleDateFormat;
+
+/**
+ * Created by shg50_000 on 6/20/2017.
+ */
+public class CalendarSettings {
+
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void processTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat();
+    }
+}
